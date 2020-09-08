f37831135ab5f684c276bdb956d77ab88e0c1765
Persunoff <mr.bblhocmo3ra@mail.ru>
2020-09-08 21:38:12 +0300

    style

--- ourproject.py
+++ ourproject.py
@@ -1,13 +1,17 @@
-def words_occur():
+def words_count():
     file_name = input('Input file name: ')
-    f=open(file_name,mode='r',encoding='ascii')
-    word_list=f.read().split()
+    f = open(file_name, mode='r')
+    word_list = f.read().split()
     f.close()
-    occurs_dict={}
+    occurs_dict = {}
+
     for word in word_list:
-        occurs_dict[word]=occurs_dict.get(word,0)+1
-        print("File %s has %d words (%d are unique)" \
-           % (file_name3, len(word_list), len(occurs_dict)))
+        occurs_dict[word] = occurs_dict.get(word, 0) + 1
+        print('File {} has {} words ({} are unique)'.format(
+            file_name, len(word_list), len(occurs_dict)
+        ))
         print(occurs_dict)
+
+
 if __name__ == '__main__':
-    words_occur()
+    words_count()
